7fd934949f8f34d3319688987f17ca99bab1c950
Amrit Raj <amritrajsingh300@gmail.com>
2026-07-28 12:24:36 +0530

    created a login page and use of axios,cors and how to connect both frontend and backend to receive cookies in browser

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
 
-export default Login
+  const [emailId,setEmailId]=useState("elon@gmail.com");
+  const [password,setPassword]=useState("Elon@123");
+
+  const handleLogin=async ()=>{
+    try{
+      const res=await axios.post("http://localhost:7777/login",{
+        emailId,
+        password,
+      },{
+        withCredentials:true
+      });
+    }
+    catch(err){
+      console.log(err);
+    }
+  };
+
+
+  return (
+    <div className="flex justify-center my-20">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset my-2">
+              <legend className="fieldset-legend">Email Id</legend>
+              <input type="text" className="input" value={emailId} onChange={(e)=>setEmailId(e.target.value)}/>
+            </fieldset>
+          </div>
+          <div>
+            <fieldset className="fieldset my-2">
+              <legend className="fieldset-legend">Password</legend>
+              <input type="text" className="input" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center m-2">
+            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
